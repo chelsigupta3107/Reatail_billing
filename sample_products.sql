43dc0a767c46415c9c6e5c04ca0dc1e30dfa0c4f
chelsigupta3107 <chelsigupta31@gmail.com>
2026-07-05 13:48:13 +0530

    Insert sample grocery products into database

---
+++ sample_products.sql
@@ -0,0 +1,8 @@
+Use joy_guru_bhandar;
+INSERT INTO products (pname, price, stock, category) VALUES
+('Rice Basmati 1kg', 95.00, 100, 'Grocery'),
+('Sugar 1kg', 45.00, 30, 'Grocery'),
+('Jaggery 500g', 55.00, 60, 'Grocery'),
+('Fortune Oil 1L', 140.00, 50, 'Grocery'),
+('Mustard Oil 1L', 165.00, 40, 'Grocery'),
+('Maida 1kg', 48.00, 70, 'Grocery');
